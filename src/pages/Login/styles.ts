8dcb1131ff2305c2f60f31fb462d6315e57f384f
Jorge Oliveira <137744314+JorgeRafael077@users.noreply.github.com>
2024-05-28 13:59:07 -0300

    Update styles.ts

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -26,9 +26,9 @@ export const LoginContainer = styled.div`
 `
 
 export const Title = styled.h1`
-    font-size: 42px;
+    font-size: 40px;
     font-weight: 700;
-    color: #81259D;
+    color: ##a52bad;
 `
 
 export const Row = styled.div`
